9a06189efb41a4b6a568a6f68d2b56afd12b3208
Lau Yi <lauyilouis@gmail.com>
2018-09-26 00:02:38 +0800

    remove background color

--- index.js
+++ index.js
@@ -136,7 +136,6 @@ export default class AutoTags extends Component {
           }
           containerStyle={this.props.containerStyle || styles.containerStyle}
           underlineColorAndroid="transparent"
-          style={{ backgroundColor: "#efeaea" }}
           listContainerStyle={{
             backgroundColor: this.props.tagsOrientedBelow
               ? "#efeaea"
